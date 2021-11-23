35812566fc30bab8e8cfdc436d7e71048f92afe7
gabrielyoon7 <gabrielyoon7@gmail.com>
2021-11-23 15:12:12 +0900

    copy

---
+++ app/src/main/java/kr/ac/kyonggi/chimpanzee_game/GameActivity.java
@@ -0,0 +1,24 @@
+package kr.ac.kyonggi.chimpanzee_game;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class GameActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        }
+    }
