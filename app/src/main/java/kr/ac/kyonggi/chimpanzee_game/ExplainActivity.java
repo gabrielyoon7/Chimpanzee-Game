cadb45e9249b75e59c4d0cf1800c3d5252561f38
gykim0923 <gykim0923@gmail.com>
2021-11-30 16:04:03 +0900

    설명화면, 메뉴 구현완료

--- app/src/main/java/kr/ac/kyonggi/chimpanzee_game/ExplainActivity.java
+++ app/src/main/java/kr/ac/kyonggi/chimpanzee_game/ExplainActivity.java
@@ -1,8 +1,10 @@
 package kr.ac.kyonggi.chimpanzee_game;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,5 +20,24 @@ public class ExplainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menubar, menu);
         return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item ){
+
+        switch (item.getItemId()){
+            case R.id.toMain:
+                Intent intent1 = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent1);
+                return true;
+            case R.id.toEnd:
+
+                return true;
+            case R.id.toStart:
+                Intent intent3 = new Intent(getApplicationContext(),GameActivity.class);
+                startActivity(intent3);
+                return true;
+
+
+        }
+        return super.onContextItemSelected(item);
     }
 }
--- app/src/main/java/kr/ac/kyonggi/chimpanzee_game/GameActivity.java
+++ app/src/main/java/kr/ac/kyonggi/chimpanzee_game/GameActivity.java
@@ -2,9 +2,11 @@ package kr.ac.kyonggi.chimpanzee_game;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -29,6 +31,25 @@ public class GameActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menubar, menu);
         return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item ){
+
+        switch (item.getItemId()){
+            case R.id.toMain:
+                Intent intent1 = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent1);
+                return true;
+            case R.id.toEnd:
+
+                return true;
+            case R.id.toStart:
+                Intent intent3 = new Intent(getApplicationContext(),GameActivity.class);
+                startActivity(intent3);
+                return true;
+
+
+        }
+        return super.onContextItemSelected(item);
     }
 
     }
