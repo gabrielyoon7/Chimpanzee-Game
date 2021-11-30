7b45bb811a15436fb48a47d827d9864896eb5970
gykim0923 <gykim0923@gmail.com>
2021-11-30 15:28:09 +0900

    설명화면, 메뉴 추가

--- app/src/main/java/kr/ac/kyonggi/chimpanzee_game/ExplainActivity.java
+++ app/src/main/java/kr/ac/kyonggi/chimpanzee_game/ExplainActivity.java
@@ -1,35 +1,22 @@
 package kr.ac.kyonggi.chimpanzee_game;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-public class GameActivity extends AppCompatActivity {
-
-
+public class ExplainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_explain);
 
-        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menubar, menu);
         return true;
     }
-
-    }
-
+}
--- app/src/main/java/kr/ac/kyonggi/chimpanzee_game/MainActivity.java
+++ app/src/main/java/kr/ac/kyonggi/chimpanzee_game/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Button startButton = (Button) findViewById(R.id.startButton);
         Button exButton = (Button) findViewById(R.id.button2);
@@ -27,9 +31,37 @@ public class MainActivity extends AppCompatActivity {
         exButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(),);
-//                startActivity(intent);
+                Intent intent = new Intent(getApplicationContext(),ExplainActivity.class);
+                startActivity(intent);
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menubar, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item ){
+
+        switch (item.getItemId()){
+            case R.id.toMain:
+                Intent intent1 = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent1);
+                return true;
+            case R.id.toEnd:
+
+                return true;
+            case R.id.toStart:
+                Intent intent3 = new Intent(getApplicationContext(),GameActivity.class);
+                startActivity(intent3);
+                return true;
+
+
+        }
+        return super.onContextItemSelected(item);
+    }
+
+
+
 }
