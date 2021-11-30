17a6b8da1104e0336d8dbff298edd0202bab46cf
gykim0923 <gykim0923@gmail.com>
2021-11-30 16:33:29 +0900

    게임화면, 게임종료화면 구현

--- app/src/main/java/kr/ac/kyonggi/chimpanzee_game/GameActivity.java
+++ app/src/main/java/kr/ac/kyonggi/chimpanzee_game/GameActivity.java
@@ -1,18 +1,10 @@
 package kr.ac.kyonggi.chimpanzee_game;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,7 +15,7 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_mode);
 
         }
     @Override
